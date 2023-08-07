b845aa0531ab322afd7d1223c4ae3ab6e9c8c7d2
Koemse Pon <koemse.pon@csx.com.kh>
2023-08-07 08:17:25 +0700

    add proxy server

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import socket
 from requests import get
+from simple_websocket_server import WebSocketServer, WebSocket
+import socketserver
+import simple_http_server
+import urllib
+import http.server
+PORT = 9097
 
 def check_ip():
     hostname = socket.gethostname()
@@ -9,7 +15,16 @@ def check_ip():
     ip = get('https://api.ipify.org').content.decode('utf8')
     print('My public IP address is: {}'.format(ip))
 
+class MyProxy(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        url=self.path[1:]
+        self.send_response(200)
+        self.end_headers()
+        self.copyfile(urllib.urlopen(url), self.wfile)
+        self.copyfile(urllib.urlopen(url), self.wfile)
 
-if __name__ == '__main__':
-    check_ip()
+httpd = socketserver.ThreadingTCPServer(('', PORT), MyProxy)
+print("Now serving at :"+str(PORT))
+httpd.serve_forever()
+
 
