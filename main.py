8b8d1ea36edcbe984e70652578c704793f5b4267
Koemse Pon <koemse.pon@csx.com.kh>
2023-08-10 11:46:03 +0700

    check os

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import socket
 from requests import get
 PORT = 9097
 from sys import platform
+import os
+
 
 def check_ip():
     hostname = socket.gethostname()
@@ -11,6 +13,8 @@ def check_ip():
     ip = get('https://api.ipify.org').content.decode('utf8')
     print('My public IP address is: {}'.format(ip))
     print(platform)
+    cmd = 'ls -l'
+    os.system(cmd)
 
 check_ip()
 
